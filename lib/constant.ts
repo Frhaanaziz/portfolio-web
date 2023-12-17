925975228b2533399348220921e8248f00e67803
Farhan Aziz <farhan@aththariq.com>
2023-12-17 14:53:20 +0700

    Update web config

--- lib/constant.ts
+++ lib/constant.ts
@@ -1,15 +1,15 @@
 import { Github, Linkedin, Mail } from 'lucide-react';
 
-export const avatarUrl = '/';
+export const avatarUrl = '/images/avatar.jpg';
 export const contactNavigation = [
   {
     name: 'GitHub',
-    href: '#',
+    href: 'https://github.com/Frhaanaziz',
     icon: Github,
   },
   {
     name: 'Linkedin',
-    href: '#',
+    href: 'https://www.linkedin.com/in/farhan-aziz-ath-thariq-4283b5240/',
     icon: Linkedin,
   },
   {
