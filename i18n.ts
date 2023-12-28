40a6b0c076ae7c3ff4e6344cac9365af5016d14c
Farhan Aziz <farhan@aththariq.com>
2023-12-28 23:00:32 +0700

    Update i18n.ts to use getIntlMessagesAction

--- i18n.ts
+++ i18n.ts
@@ -1,8 +1,13 @@
 import { getRequestConfig } from 'next-intl/server';
+import { getIntlMessagesAction } from './app/_actions';
 
+// @ts-ignore
 export default getRequestConfig(async ({ locale }) => ({
-  messages: (await import(`./messages/${locale}.json`)).default,
+  messages: (await getIntlMessagesAction({ locale })).data,
 }));
+// export default getRequestConfig(async ({ locale }) => ({
+//   messages: (await import(`./messages/${locale}.json`)).default,
+// }));
 // export default getRequestConfig(async ({ locale }) => ({
 //   messages: {},
 // }));
