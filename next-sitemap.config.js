863615e096bc5cfe496aa54724c3c13891116e6d
Farhan Aziz <farhan@aththariq.com>
2024-02-02 14:12:31 +0700

    Add next-sitemap.config.js file

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+const env = require('./env.mjs');
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: env.NEXT_PUBLIC_BASE_URL,
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
