0896cad7512ca279b0d77e438081e67c8d1b88f0
Farhan Aziz <farhan@aththariq.com>
2024-02-03 22:53:14 +0700

    Update siteUrl and generateRobotsTxt in next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,9 +1,5 @@
-const env = require('./env.mjs');
-// import { env } from './env.mjs';
-
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: env.NEXT_PUBLIC_BASE_URL,
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  siteUrl: 'https://aththariq.com',
+  generateRobotsTxt: true,
 };
