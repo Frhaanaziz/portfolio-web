4935facb7b20a5cf7e96bad8019c45286f1c0927
Farhan Aziz <farhan@aththariq.com>
2024-04-22 22:58:58 +0700

    Remove getIntlMessagesAction import in i18n.ts

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,4 @@
 import { getRequestConfig } from 'next-intl/server';
-import { getIntlMessagesAction } from './app/_actions';
 
 // export default getRequestConfig(async ({ locale }) => ({
 //   messages: (await getIntlMessagesAction({ locale })).data ?? {},
