7db6d5170c23570719f707e39a15cb24b2089a16
Farhan Aziz <farhan@aththariq.com>
2024-04-22 22:56:44 +0700

    Refactor utils.ts to remove unused code and export array constants

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,16 +6,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const array1 = Array.from({ length: 1 }, (_, i) => i + 1);
-export const array2 = Array.from({ length: 2 }, (_, i) => i + 1);
-export const array3 = Array.from({ length: 3 }, (_, i) => i + 1);
-export const array4 = Array.from({ length: 4 }, (_, i) => i + 1);
-export const array5 = Array.from({ length: 5 }, (_, i) => i + 1);
-export const array6 = Array.from({ length: 6 }, (_, i) => i + 1);
-export const array7 = Array.from({ length: 7 }, (_, i) => i + 1);
-export const array8 = Array.from({ length: 8 }, (_, i) => i + 1);
-export const array9 = Array.from({ length: 9 }, (_, i) => i + 1);
-
 export function getZodErrorMessage(result: z.SafeParseError<any>) {
   let errorMessage = '';
 
